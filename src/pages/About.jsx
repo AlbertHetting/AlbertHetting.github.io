9b107be97a7dc6c5aae39b8f002669b39c5936ef
Albert Hetting <Albertlund@hotmail.dk>
2025-09-17 19:28:20 +0200

    About section tester

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,15 @@
-export default function About() {}
+import "../components/about.css"
+
+export default function About() {
+
+
+return(
+    <section className="PortfolioPicCon">
+        <div className="PortfolioPic">
+        <img id="mainimage" src="videos/PortfolioPicture5.jpg" alt="" />
+        </div>
+      </section>
+
+    );
+
+}
