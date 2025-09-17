9313747c7e0dba792f51614918f6d5488a7c606c
Albert Hetting <Albertlund@hotmail.dk>
2025-09-17 19:31:11 +0200

    image fix withbase

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,11 +2,16 @@ import "../components/about.css"
 
 export default function About() {
 
+const withBase = (p="") => {
+  const base = import.meta.env.BASE_URL || "/";
+  return base + p.replace(/^public\//, "").replace(/^\/?/, "");
+};
+
 
 return(
     <section className="PortfolioPicCon">
         <div className="PortfolioPic">
-        <img id="mainimage" src="videos/PortfolioPicture5.jpg" alt="" />
+        <img id="mainimage" src={withBase("videos/PortfolioPicture5.jpg")} alt="" />
         </div>
       </section>
 
